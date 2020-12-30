bab8419f9babe71208898720da2d8affe616554c
Cmac <mckoyc@gmail.com>
2020-12-30 13:35:49 -0500

    added navabar to homepage

--- Develop/routes/html-routes.js
+++ Develop/routes/html-routes.js
@@ -2,6 +2,6 @@ const path = require("path");
 
 module.exports = (app)=>{
     app.get("/", (req, res)=>{ 
-        res.sendFile(path.join(__dirname, "../public/index.html"))
+        res.sendFile(path.join(__dirname, "../public/homepage.html"))
     })
 }
