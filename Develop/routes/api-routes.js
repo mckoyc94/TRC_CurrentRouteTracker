82f84474eb16f9335fdbe223334f34b780467789
Cmac <mckoyc@gmail.com>
2021-01-28 13:58:12 -0500

    added api routes for boulder and top rope gets and creates

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -4,7 +4,29 @@ const {Boulder, TopRope} = db
 module.exports = (app) => {
     app.get('/api/boulders', (req, res) => {
         Boulder.find({}) 
-    }).then((dbBoulder) => {
-        res.json(dbBoulder)
+        .then(dbBoulder => {
+            res.json(dbBoulder)
+        })
+    })
+
+    app.post('/api/boulders', ({body}, res) => {
+        Boulder.create(body)
+        .then(dbBoulder => {
+            res.json(dbBoulder)
+        })
+    })
+
+    app.get('/api/topRope', (req, res) => {
+        TopRope.find({})
+        .then(dbTR => {
+            res.json(dbTR)
+        })
+    })
+
+    app.post('/api/topRope', ({body}, res) => {
+        TopRope.create(body)
+        .then(dbTR => {
+            res.json(dbTR)
+        })
     })
 }
