e98266dce4e400aeb542dbcc1cfadbacd1b89d6e
Cmac <mckoyc@gmail.com>
2021-02-03 14:15:23 -0500

    added basis for table chart

--- Develop/public/js/homepage.js
+++ Develop/public/js/homepage.js
@@ -1,6 +1,23 @@
-const hello = $('h1')
-const button = $('button')
-button.on('click', () => {
-    console.log("clicked")
-    hello.css("color", "red")
-} )
+const date = $('.card-subtitle')
+const ctx = document.getElementById('myChart').getContext('2d');
+
+date.text("Testing Weather")
+
+const chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['7', '8', '9', '10a', '10b', '10c', '10d', '11a', '11b', '11c', '11d', '12a', '12b', '12c', '12d', '13a'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [4, 7, 9, 11, 8, 10, 4, 5, 9, 7, 5, 2, 2]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
