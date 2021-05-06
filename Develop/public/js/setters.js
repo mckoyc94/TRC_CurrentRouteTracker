7bb05e6a0a9d00266200a80e8402fc4887a5efbf
Cmac <mckoyc@gmail.com>
2021-05-06 10:37:34 -0400

    added semantic language to setters.js page

--- Develop/public/js/setters.js
+++ Develop/public/js/setters.js
@@ -66,75 +66,93 @@ const loadSetters = () => {
     })    
 }
 
+// Changes Active Status for Setter
 const changeActive = (active, setter) => {
+    // Checks Which Change is Requested
     if (active === 'activate'){
+        // Reactivates Setter
         const oldSetter = { 
             name: setter,
             active: true
         }
+        // Updates Setter in DB
         $.post('/api/setters/update', oldSetter)
+        // Confirms Change for User
         .then(alert(`${oldSetter.name} reactivated`))
+        // Reloads Lists
         .then(getSetters())
             
     } else if (active === 'deactivate'){
+        // Deactivates Setter
         const oldSetter = { 
             name: setter,
             active: false
         }
+        // Updates Setter in DB
         $.post('/api/setters/update', oldSetter)
+        // Confirms Changes for User
         .then(alert(`${oldSetter.name} deactivated`))
+        // Reloads Lists
         .then(getSetters())
     }
 }
 
+// Reactivates Old Setter
 $("#reactive").on("click", e => {
     e.preventDefault()
     const setName = $('#inputSetter').val()
     changeActive("activate", setName)
 })
 
+// Deactivates Current Setter
 setterList.on("click", "button", e => {
     e.preventDefault()
     const setName = e.target.id 
     changeActive("deactivate", setName)
 })
 
+// Adds New Setter to DB
 $("#addSet").on('click', e => {
     e.preventDefault()
+    // Corrects Name for first letters of each name to be capitalized
     const input = $('#inputName').val().split(" ")
     let newName = ""
-
     input.forEach(word => {
         word = word.charAt(0).toUpperCase() + word.slice(1)
         newName = newName + word + " "
     })
-
+    // Body for New Setter
     const newSetter = {
         name: newName.trim(),
         initials: $('#inputInitial').val().toUpperCase()
     }
-
+    // Destructures Setter Obj
     const {name, initials} = newSetter
 
+    // Makes sure the user filled out all necessary forms
     if(name === '' || initials === ''){
         alert("Please Add a Name and Initials")
     } else {
+        // Goes through Database to confirm a New Setter is being added
         $.get('/api/setters').then(data => {
             const settersDB = [...data]
             let oldSet;
             let oldSetInit;
+            // Filters through Setters and returns any matches
             oldSet = settersDB.filter(setter => setter.name.toUpperCase() === name.toUpperCase())
             oldSetInit = settersDB.filter(setter => setter.initials === initials)
 
-            console.log(oldSet, oldSetInit)
-
+            // If any matches found, asks user to add new Setter
             if(oldSetInit.length != 0){
                 alert("A Setter Already has these initials")
             } else if (oldSet.length != 0){
                 alert("There is Already a Setter with this name")
             } else {
+               // Posts new Setter to DB
                $.post('/api/setters', newSetter)
+               // Confirms entry for user
                .then(alert(`Welcome our newest setter: ${name}`))
+               // Resets page and text box for another setter
                .then(() => {
                    location.reload()
                 })
@@ -144,6 +162,6 @@ $("#addSet").on('click', e => {
     }
 })
 
-
+// Loads Setters on Page Load
 getSetters()
 
